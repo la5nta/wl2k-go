9365c182ad54f82cbaf9784db88f333716730a3d
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2017-02-01 21:47:04 +0100

    ardop: Support parsing of both ARDOP TNCs IDF format
    Apparently, ARDOP_Win and ARDOPc differs slightly in IDF frame format.
    
    Issue #45

--- transport/ardop/frame_test.go
+++ transport/ardop/frame_test.go
@@ -3,19 +3,27 @@ package ardop
 import "testing"
 
 func TestParseIDFrame(t *testing.T) {
-	frame := dFrame{
-		dataType: `IDF`,
-		data:     []byte(` ID LA5NTA:[JP20QE] `),
+	frames := []dFrame{
+		{
+			dataType: `IDF`,
+			data:     []byte(` ID LA5NTA:[JP20QE] `),
+		},
+		{
+			dataType: `IDF`,
+			data:     []byte(` LA5NTA:[JP20QE] `),
+		},
 	}
 
-	call, loc, err := parseIDFrame(frame)
-	if err != nil {
-		t.Fatalf("Unexpected parse error: %s", err)
-	}
-	if call != "LA5NTA" {
-		t.Errorf("Unexpected call: %s", call)
-	}
-	if loc != "JP20QE" {
-		t.Errorf("Unexpected locator: %s", loc)
+	for _, frame := range frames {
+		call, loc, err := parseIDFrame(frame)
+		if err != nil {
+			t.Fatalf("Unexpected parse error: %s", err)
+		}
+		if call != "LA5NTA" {
+			t.Errorf("Unexpected call: %s", call)
+		}
+		if loc != "JP20QE" {
+			t.Errorf("Unexpected locator: %s", loc)
+		}
 	}
 }
